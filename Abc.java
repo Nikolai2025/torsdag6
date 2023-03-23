bef82682f380785200379e70a458eb0281c58c41
Nikolai2025 <124783539+Nikolai2025@users.noreply.github.com>
2023-03-23 20:57:35 +0100

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,30 @@
+public class Abc {
+    public static void methodA() {
+        System.out.println("j");
+        System.out.println("a");
+        System.out.println("v");
+        System.out.println("a");
+    }
+
+    public static void methodB() {
+        System.out.println("e");
+        System.out.println("r");
+
+    }
+
+    public static void methodC() {
+        System.out.println("s");
+        System.out.println("j");
+        System.out.println("o");
+
+    }
+
+
+    public static void methodD() {
+        System.out.println("v");
+        System.out.println("t");
+    }
+}
+
+
+
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      Abc.methodA();
+      Abc.methodB();
+      Abc.methodC();
+      Abc.methodD();
+
+    }
+}
